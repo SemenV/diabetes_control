e41b466712d5d455ef59abed3650e02889280b85
SemenV <semen.vinn@mail.ru>
2023-01-21 18:09:45 +0300

    отправляет углаводы в ответ

--- start.py
+++ start.py
@@ -1,26 +1,22 @@
 from flask import Flask, request,jsonify
+import requests
+import json
 
 app = Flask(__name__)
 
+with open("zapr.json", "r") as my_file:
+    zapr_json = my_file.read()
 
-
-testt = {
-    
-    "response": {
-      "text": "Привет - это ответ",
-      "end_session": "false"
-    },
-    "version": "1.0"
- 
-}
-
+zapr = json.loads(zapr_json)
    
 @app.route("/", methods=['POST'])
 def indexx():
     request_data = request.get_json()
     comm = request_data["request"]["command"]
-    testt["response"]["text"] = comm
-    return jsonify(testt)
+    
+    r = requests.get("https://ru-ru.openfoodfacts.org/category/" + comm + "/1.json")
+    zapr["response"]["text"] = r.json()["products"][1]["nutriments"]["carbohydrates"]
+    return jsonify(zapr)
     
 if __name__ == "__main__":
     app.run(dubug=False, host='0.0.0.0')
